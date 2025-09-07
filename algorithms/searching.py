32620fe6364c85ffdbe96e3df3d1a41bc20b7b68
dheerajsinghnagdali <dheerajsinghnagdali@gmail.com>
2025-09-07 17:14:09 +0530

    feat: add linear search implementation and refactor binary search parameter

--- algorithms/searching.py
+++ algorithms/searching.py
@@ -1,13 +1,19 @@
-def binary_search(nums, value):
+def linear_search(lst, target):
+    for i in range(len(lst)):
+        if lst[i] == target:
+            return i
+
+
+def binary_search(nums, target):
     if len(nums) == 0:
         return None
 
     midIdx = len(nums) // 2
     mid = nums[midIdx]
 
-    if mid == value:
+    if mid == target:
         return midIdx
-    elif mid > value:
-        return binary_search(nums[:midIdx], value)
+    elif mid > target:
+        return binary_search(nums[:midIdx], target)
     else:
-        return binary_search(nums[midIdx + 1 :], value)
+        return binary_search(nums[midIdx + 1 :], target)
